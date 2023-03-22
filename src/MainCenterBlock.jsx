ada10918480ffc3090bb8aa1aa3db88850b3baab
ZakirovaAK <zakirova.alina116@gmail.com>
2023-03-22 17:08:38 +0300

    черновики изменений

--- src/MainCenterBlock.jsx
+++ src/MainCenterBlock.jsx
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
 import CenterBlock from './CenterBlock';
 import PlayListContent from './TrackList';
 
+function FilterButton({ text, isClicked }) {
+  return (
+    <div
+      className={
+        isClicked
+          ? 'filter__button filter__button__clicked _btn-text'
+          : 'filter__button _btn-text'
+      }
+    >
+      {text}
+    </div>
+  )
+}
+
 function MainCenterBlock(props) {
+  // Объявляем state, функцию для его изменения и изначальное значение
+  const [isClickedAuthor, setisClickedAuthor] = useState(false);
+
   return (
     <div className="main__centerblock centerblock">
       <div className="centerblock__search search">
@@ -18,9 +36,10 @@ function MainCenterBlock(props) {
       <h2 className="centerblock__h2">Треки</h2>
       <div className="centerblock__filter filter">
         <div className="filter__title">Искать по:</div>
-        <div className="filter__button button-author _btn-text">
+        {/* <div className="filter__button button-author _btn-text">
           исполнителю
-        </div>
+        </div> */}
+        <FilterButton text="исполнителю" isClicked=isClickedAuthor/>
         <div className="filter__button button-year _btn-text">году выпуска</div>
         <div className="filter__button button-genre _btn-text">жанру</div>
       </div>
