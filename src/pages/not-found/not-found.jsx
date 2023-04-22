d7901fb17b59c0cc15ff09cecb28f415e5fd5440
ZakirovaAK <zakirova.alina116@gmail.com>
2023-04-22 23:07:38 +0300

    Not Found page

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import NotFound from '../../components/not-found/not-found';
-import * as Styled from './not-found.style';
+import React from 'react'
+import NotFound from '../../components/not-found/not-found'
+import * as Styled from './not-found.style'
 
 const NotFoundPage = () => {
-    return (
-        <Styled.NotFoundPage>
-            <NotFound />
-        </Styled.NotFoundPage>
-    );
-};
+  return (
+    <Styled.WrapperNotFound>
+      <Styled.NotFoundPage>
+        <NotFound />
+      </Styled.NotFoundPage>
+    </Styled.WrapperNotFound>
+  )
+}
 
-export default NotFoundPage;
+export default NotFoundPage
--- src/pages/not-found/not-found.style.js
+++ src/pages/not-found/not-found.style.js
@@ -6,3 +6,10 @@ export const NotFoundPage = styled.div`
     justify-content: center;
     height: 100vh;
 `;
+
+export const WrapperNotFound = styled.div`
+  width: 100%;
+  min-height: 100%;
+  overflow: hidden;
+  background-color: #181818;
+`
